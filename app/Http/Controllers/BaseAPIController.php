084355ef11fc910163d93b924c5452aced9d2249
rizwar <rizwar.adnanbyantara@gmail.com>
2023-04-18 18:13:47 +0700

    Menggunakan Request di body untuk mengirim tenant_id dan report_name

--- app/Http/Controllers/BaseAPIController.php
+++ app/Http/Controllers/BaseAPIController.php
@@ -7,11 +7,11 @@ use Illuminate\Http\Request;
 
 class BaseAPIController extends BaseResponseController
 {
-    public function getResponse($report_name){
-        $search_data = Query::where('report_name','=',$report_name)->first();
+    public function getResponse(Request $request){
+        $search_data = Query::where('report_name','=',$request->report_name)->first();
 
         if(empty($search_data)){
-            return $this->sendResponse($search_data,"Empty!");
+            return $this->sendResponse($search_data,null);
         }
 
         return $this->sendResponse($search_data,"Success!");
--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/getOne/{id}', [BaseAPIController::class, 'getResponse']);
+Route::get('/getOne', [BaseAPIController::class, 'getResponse']);
